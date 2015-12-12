a88a53ed0021931a7f13230b6e9a62219c4acae5
Ethan Kennerly <ethan@lumoslabs.com>
2015-12-12 14:45:38 -0800

    If collide, divide speed.

--- Assets/Scripts/Model.cs
+++ Assets/Scripts/Model.cs
@@ -40,5 +40,6 @@ public class Model
 		{
 			vehicles[index].Update(deltaSeconds);
 		}
+		player.UpdateCollision(vehicles, playerIndex);
 	}
 }
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -1,3 +1,5 @@
+using UnityEngine;  // Mathf
+
 public class Vehicle
 {
 	public float x = 0.0f;
@@ -5,9 +7,45 @@ public class Vehicle
 	public float z = 0.0f;
 
 	public float speed = 0.0f;
+	public float collisionRadius = 0.2f;
+	public float collisionSpeedMultiplier = 0.05f;
+	public bool isColliding;
+	public bool isCollidingNow;
+	private bool wasColliding;
 
 	public void Start()
 	{
+	}
+
+	public bool IsColliding(Vehicle other)
+	{
+		float intersect = collisionRadius + other.collisionRadius;
+		isColliding = (other.speed < speed) &&
+			(Mathf.Abs(z - other.z) < intersect
+			|| Mathf.Abs(x - other.x) < intersect);
+		return isColliding;
+	}
+
+	public bool UpdateCollision(Vehicle[] vehicles, int index)
+	{
+		wasColliding = isColliding;
+		int ahead = index - 1;
+		int behind = index + 1;
+		if (0 <= ahead)
+		{
+			isColliding = IsColliding(vehicles[ahead]);
+		}
+		if (!isCollidingNow && behind < vehicles.Length)
+		{
+			isColliding = IsColliding(vehicles[behind]);
+		}
+		isCollidingNow = !wasColliding && isColliding;
+		if (isCollidingNow)
+		{
+			speed *= collisionSpeedMultiplier;
+			// Debug.Log("Vehicle.UpdateCollision: speed " + speed);
+		}
+		return isCollidingNow;
 	}
 
 	public void Update(float deltaSeconds)
